d451d450a068ae27e9a9ab8233bf1d5d43245a4d
nakana <t-nakanishi@esm.co.jp>
2016-03-27 01:20:43 +0900

    clojure

---
+++ challenge_004_data_conv/nakana/clojure/kadai/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kadai
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ challenge_004_data_conv/nakana/clojure/kadai/src/kadai/core.clj
@@ -0,0 +1,31 @@
+(ns kadai.core)
+
+(def data [
+           {:hosp "a" :data  1 }
+           {:hosp "a" :data  2 }
+           {:hosp "a" :data  3 }
+           {:hosp "b" :data  1 }
+           {:hosp "b" :data  2 }
+           {:hosp "b" :data  3 }
+           {:hosp "c" :data  1 }
+           {:hosp "c" :data  2 }
+           {:hosp "c" :data  3 }
+           ])
+
+;; (def tmp (group-by :hosp data))
+;; (def result
+;;   (reduce-kv (fn [m k v]
+;;                (conj m (assoc {} :hosp k :data (map :data v))))
+;;              []
+;;              tmp))
+
+(def reuslt
+  (->> data
+       (group-by :hosp)
+       (reduce-kv (fn [m k v]
+                    (conj m (assoc {} :hosp k :data (map :data v))))
+                  []
+                  )))
+
+
+
